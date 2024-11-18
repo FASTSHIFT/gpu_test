f5b88dd2718a9cea82ec83c955863f6fcbf75829
_VIFEXTech <vifextech@foxmail.com>
2024-11-18 11:51:41 +0800

    chore(gpu_recorder): complete code style switching

--- gpu_recorder.c
+++ gpu_recorder.c
@@ -1,126 +1,110 @@
-/****************************************************************************
- * apps/testing/gpu/gpu_recorder.c
+/*
+ * MIT License
+ * Copyright (c) 2023 - 2024 _VIFEXTech
  *
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.  The
- * ASF licenses this file to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance with the
- * License.  You may obtain a copy of the License at
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations
- * under the License.
- *
- ****************************************************************************/
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
-/****************************************************************************
- * Included Files
- ****************************************************************************/
+/*********************
+ *      INCLUDES
+ *********************/
 
 #include "gpu_recorder.h"
 #include "gpu_test.h"
+#include <errno.h>
 #include <fcntl.h>
-#include <string.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
+#include <string.h>
+#include <unistd.h>
 
-/****************************************************************************
- * Pre-processor Definitions
- ****************************************************************************/
+/*********************
+ *      DEFINES
+ *********************/
 
-/****************************************************************************
- * Private Types
- ****************************************************************************/
+/**********************
+ *      TYPEDEFS
+ **********************/
 
-struct gpu_recorder_s
-{
-  struct gpu_test_context_s *ctx;
-  int fd;
+struct gpu_recorder_s {
+    struct gpu_test_context_s* ctx;
+    int fd;
 };
 
-/****************************************************************************
- * Private Data
- ****************************************************************************/
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
 
-/****************************************************************************
- * Public Functions
- ****************************************************************************/
-
-/****************************************************************************
- * Name: gpu_recorder_create
- ****************************************************************************/
-
-struct gpu_recorder_s *gpu_recorder_create(
-    struct gpu_test_context_s *ctx,
-    const char *name)
+struct gpu_recorder_s* gpu_recorder_create(struct gpu_test_context_s* ctx, const char* name)
 {
-  struct gpu_recorder_s *recorder;
-  char path[256];
-  char filename[64];
-  gpu_get_localtime_str(filename, sizeof(filename));
+    struct gpu_recorder_s* recorder;
+    char path[256];
+    char filename[64];
+    gpu_get_localtime_str(filename, sizeof(filename));
 
-  snprintf(path, sizeof(path), "%s/report_%s_%s.csv",
-           ctx->param.output_dir, name, filename);
+    snprintf(path, sizeof(path), "%s/report_%s_%s.csv",
+        ctx->param.output_dir, name, filename);
 
-  int fd = open(path, O_CREAT | O_WRONLY | O_CLOEXEC, 0666);
-
-  if (fd < 0)
-    {
-      GPU_LOG_ERROR("open %s failed", path);
-      return NULL;
+    int fd = open(path, O_CREAT | O_WRONLY | O_CLOEXEC, 0666);
+    if (fd < 0) {
+        GPU_LOG_ERROR("open %s failed: %d", path, errno);
+        return NULL;
     }
 
-  recorder = calloc(1, sizeof(struct gpu_recorder_s));
-  GPU_ASSERT_NULL(recorder);
-  recorder->fd = fd;
-  recorder->ctx = ctx;
-  GPU_LOG_WARN("recorder file: %s created, fd = %d", path, fd);
-  return recorder;
+    recorder = calloc(1, sizeof(struct gpu_recorder_s));
+    GPU_ASSERT_NULL(recorder);
+    recorder->fd = fd;
+    recorder->ctx = ctx;
+    GPU_LOG_INFO("recorder file: %s created, fd = %d", path, fd);
+    return recorder;
 }
 
-/****************************************************************************
- * Name: gpu_recorder_delete
- ****************************************************************************/
-
-void gpu_recorder_delete(struct gpu_recorder_s *recorder)
+void gpu_recorder_delete(struct gpu_recorder_s* recorder)
 {
-  GPU_ASSERT_NULL(recorder);
-  close(recorder->fd);
-  free(recorder);
-  GPU_LOG_WARN("recorder deleted");
+    GPU_ASSERT_NULL(recorder);
+    close(recorder->fd);
+    memset(recorder, 0, sizeof(struct gpu_recorder_s));
+    free(recorder);
+    GPU_LOG_INFO("recorder deleted");
 }
 
-/****************************************************************************
- * Name: gpu_recorder_write_string
- ****************************************************************************/
-
-int gpu_recorder_write_string(struct gpu_recorder_s *recorder,
-                              const char *str)
+int gpu_recorder_write_string(struct gpu_recorder_s* recorder, const char* str)
 {
-  GPU_ASSERT_NULL(recorder);
-  GPU_ASSERT_NULL(str);
-  size_t len = strlen(str);
-  size_t written = 0;
+    GPU_ASSERT_NULL(recorder);
+    GPU_ASSERT_NULL(str);
+    size_t len = strlen(str);
+    size_t written = 0;
 
-  while (written < len)
-    {
-      ssize_t ret = write(recorder->fd, str + written, len - written);
+    while (written < len) {
+        ssize_t ret = write(recorder->fd, str + written, len - written);
 
-      if (ret < 0)
-        {
-          GPU_LOG_ERROR("write failed: %d", errno);
-          return -1;
+        if (ret < 0) {
+            GPU_LOG_ERROR("write failed: %d", errno);
+            return -1;
         }
 
-      written += ret;
+        written += ret;
     }
 
-  return 0;
+    return 0;
 }
+
+/**********************
+ *   STATIC FUNCTIONS
+ **********************/
--- gpu_recorder.h
+++ gpu_recorder.h
@@ -1,79 +1,64 @@
-/****************************************************************************
- * apps/testing/gpu/gpu_recorder.h
+/*
+ * MIT License
+ * Copyright (c) 2023 - 2024 _VIFEXTech
  *
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.  The
- * ASF licenses this file to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance with the
- * License.  You may obtain a copy of the License at
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations
- * under the License.
- *
- ****************************************************************************/
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
-#ifndef __APPS_TESTING_GPU_RECORDER_H
-#define __APPS_TESTING_GPU_RECORDER_H
+#ifndef GPU_RECORDER_H
+#define GPU_RECORDER_H
 
-/****************************************************************************
- * Included Files
- ****************************************************************************/
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-#include <stddef.h>
+/*********************
+ *      INCLUDES
+ *********************/
 
-/****************************************************************************
- * Pre-processor Definitions
- ****************************************************************************/
+/*********************
+ *      DEFINES
+ *********************/
 
-/****************************************************************************
- * Public Types
- ****************************************************************************/
+/**********************
+ *      TYPEDEFS
+ **********************/
 
 struct gpu_test_context_s;
 struct gpu_recorder_s;
 
-/****************************************************************************
- * Public Function Prototypes
- ****************************************************************************/
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+struct gpu_recorder_s* gpu_recorder_create(struct gpu_test_context_s* ctx, const char* name);
+
+void gpu_recorder_delete(struct gpu_recorder_s* recorder);
+
+int gpu_recorder_write_string(struct gpu_recorder_s* recorder, const char* str);
+
+/**********************
+ *      MACROS
+ **********************/
 
 #ifdef __cplusplus
-#define EXTERN extern "C"
-extern "C"
-{
-#else
-#define EXTERN extern
+} /*extern "C"*/
 #endif
 
-/****************************************************************************
- * Name: gpu_recoder_create
- ****************************************************************************/
-
-struct gpu_recorder_s *gpu_recorder_create(
-    struct gpu_test_context_s *ctx,
-    const char *name);
-
-/****************************************************************************
- * Name: gpu_recoder_delete
- ****************************************************************************/
-
-void gpu_recorder_delete(struct gpu_recorder_s *recorder);
-
-/****************************************************************************
- * Name: gpu_recorder_write_string
- ****************************************************************************/
-
-int gpu_recorder_write_string(struct gpu_recorder_s *recorder,
-                              const char *str);
-
-#undef EXTERN
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* __APPS_TESTING_GPU_VGLITE_H */
+#endif /*GPU_RECORDER_H*/
